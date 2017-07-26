fd9cf4a0d49cb98f9b377fdcf66893282e6cea8f
Sean Hoyt <seanhoyt963@gmail.com>
2017-07-26 08:18:12 -0400

    chagallltetmo: Switch to the data_only lineage config
    Remove aosp telephony include as well because we do not support dialer
    and releated apps it includes
    
    Change-Id: Ic1e3346c196348a9a95577bfeeb60fd1f433c42c

--- lineage.mk
+++ lineage.mk
@@ -1,6 +1,5 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony.mk)
 
 $(call inherit-product, device/samsung/chagallltetmo/device.mk)
 
@@ -8,7 +7,7 @@ $(call inherit-product, device/samsung/chagallltetmo/device.mk)
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
 # Inherit more LineageOS stuff.
-$(call inherit-product, vendor/cm/config/telephony.mk)
+$(call inherit-product, vendor/cm/config/data_only.mk)
 
 PRODUCT_NAME := lineage_chagallltetmo
 PRODUCT_DEVICE := chagallltetmo
