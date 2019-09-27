41a5db636c607a67e62f37156d2c92cf1b9f8bba
Thomas Patry <thompatry@gmail.com>
2019-09-27 15:34:43 -0500

    chagallltetmo: Consider lineage_chagallltetmo product via the AOSP way
    Change-Id: I09dbd4bb6f70794bc63c5419c5a6935a114107a7

---
+++ lineage_chagallltetmo.mk
@@ -0,0 +1,45 @@
+#
+# Copyright (C) 2014-2016 The CyanogenMod Project
+# Copyright (C) 2017-2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Inherit from chagallltetmo device
+$(call inherit-product, device/samsung/chagallltetmo/device.mk)
+
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_MODEL := SM-T807T
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+
+# Inherit more LineageOS stuff.
+$(call inherit-product, vendor/cm/config/data_only.mk)
+
+PRODUCT_NAME := lineage_chagallltetmo
+PRODUCT_DEVICE := chagallltetmo
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_MODEL=SM-T807T \
+    PRODUCT_NAME=chagallltetmo \
+    PRODUCT_DEVICE=chagallltetmo \
+    TARGET_DEVICE=chagallltetmo \
+    BUILD_FINGERPRINT=samsung/chagallltetmo/chagallltetmo:5.0.2/LRX22G/T807TUVS1BPL2:user/release-keys \
+    PRIVATE_BUILD_DESC="chagallltetmo-user 5.0.2 LRX22G T807TUVS1BPL2 release-keys"
